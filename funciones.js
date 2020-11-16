5052359a9cc091deca901e3444152b8efd0edcf6
alexrichard1994 <alexandrorichard@hotmail.com>
2020-11-16 14:45:32 -0500

    commit

---
+++ funciones.js
@@ -0,0 +1,10 @@
+function comparar(){
+    const primero = Number(prompt("Primer Valor"));
+    const segundo= Number(prompt("Segundo Valor"));
+}
+
+if (primero>segundo){
+    alert(primero + " es mayor que " + segundo);
+}else{
+    alert(segundo + " es mayor que " + primero);
+}
